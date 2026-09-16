ea4129ac711fe4c279edcfc1eed277c9576d8099
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-16 23:11:23 +0100

    Add Pet migration and model

---
+++ app/Models/Pet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    protected $fillable = ['name', 'birth_date', 'species_id', 'user_id'];
+
+    public function species()
+    {
+        return $this->belongsTo(Species::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
--- app/Models/Species.php
+++ app/Models/Species.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Species extends Model
 {
     protected $fillable = ['name'];
+
+    public function pets()
+    {
+        return $this->hasMany(Pet::class);
+    }
 }
