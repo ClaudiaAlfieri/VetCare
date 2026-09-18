2a3d63ae05960520bbe5fda97754f6c57b8cc99e
Ricardo Lopes <Ricardo.Lopes.T0133539@edu.atec.pt>
2026-09-18 11:30:20 +0100

    Add veterinarians index view (raw HTML to Blade)

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::view('/pets', 'pets.index');
 Route::view('/pets/1', 'pets.show');
 Route::view('/pets/create', 'pets.create');
 Route::view('/pets/1/edit', 'pets.edit');
+Route::view( uri: '/veterinarians', view: 'veterinarians.index'); // /veterinarians
