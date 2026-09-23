a60ebf9aa7755c62730747c5f447e703e9699918
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-23 20:09:00 +0100

    Add soft deletes to Pet model

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pet extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['name', 'birth_date', 'species_id', 'user_id'];
 
     public function species()
